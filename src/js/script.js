ede283d830c07b67e72c67545a9448c6416f129a
Yatoom <jmapvhoof@gmail.com>
2016-06-09 00:33:23 +0200

    Reformatting css.

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,10 @@ function show(elem, bool) {
     if (bool) {
         $(elem.getElementsByTagName('ul')[0]).show();
     }
-    $(elem).addClass('highlight');
 }
 
 function hide(elem, bool) {
     if (bool) {
         $(elem.getElementsByTagName('ul')[0]).hide();
     }
-    $(elem).removeClass('highlight');
 }
