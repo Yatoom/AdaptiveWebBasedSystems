a68f47f4d3aad5bce95a29c37631608b5a164b30
j.vanhoof <j.vanhoof@ilysian.com>
2016-06-16 12:12:57 +0200

    Setup scroll effects.

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,14 @@ function hide(elem, bool) {
         $(elem.getElementsByTagName('ul')[0]).hide();
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Reveal images on scroll
+  window.sr = ScrollReveal({reset : true,});
+  sr.reveal('img');
+
+  // Make menu show up on scroll up, but hide on scroll down (for later, have to fix a few things first)
+  // var header = document.querySelector("header");
+  // var headroom  = new Headroom(header);
+  // headroom.init(); 
+});
